0575233cb99a1b6c2e899c6c57082fd51bfc1341
Vaibhav-RP <vrp8698231262@gmail.com>
2023-06-18 18:18:40 +0530

    update

--- src/test/java/com/example/awsstorage/AwsstorageApplicationTests.java
+++ src/test/java/com/example/awsstorage/AwsstorageApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.awsstorage;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AwsstorageApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
